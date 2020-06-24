d33d5d6bb490fbb190975d439a1f13f5a5437383
Joseph Adams <joseph.adams@blueyonder.co.uk>
2020-06-24 05:36:07 +0100

    fixing paths

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 virtualenv -p `which python3` venv
 source venv/bin/activate
-pip install -r requirements.txt
-python setup.py develop
-python rest_api_demo/app.py
+pip install -r api/requirements.txt
+python api/setup.py develop
+python api/rest_api_demo/app.py
