f39f1d67d3c8377af30cded78fa01261ba4988e4
Joseph Adams <joseph.adams@blueyonder.co.uk>
2020-06-24 07:07:17 +0100

    revert

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,4 @@
 virtualenv -p `which python3` venv
 source venv/bin/activate
-pip install -r ../api/requirements.txt
-python ../api/setup.py develop
-python ../api/rest_api_demo/app.py
+pip install -r api/requirements.txt
+python api/setup.py develop
